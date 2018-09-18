c3ff458b35992e48b4eb24328cd0bbd6dca40059
roel0064 <roel0064@hz.nl>
2018-09-18 10:42:52 +0200

    dope U-Test

--- UnitTest/UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest/UnitTest1.cs
@@ -6,8 +6,9 @@ namespace UnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public bool IsPrime(int candidate)
         {
+            return false;
         }
     }
 }
